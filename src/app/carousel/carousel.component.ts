3d27c91f38cb53ee513b3f81276afd3d8fc27481
Malcolm Gourdine <malcolmgourdine@gmail.com>
2017-07-12 11:31:42 -0400

    carousel styling

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {OwlCarousel} from 'ngx-owl-carousel';
 
 @Component({
   selector: 'app-carousel',
@@ -6,14 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./carousel.component.css']
 })
 export class CarouselComponent implements OnInit {
-  heroes = [
-    {id: 1, name:'Superman'},
-    {id: 2, name:'Batman'},
-    {id: 5, name:'BatGirl'},
-    {id: 3, name:'Robin'},
-    {id: 4, name:'Flash'}
+  @ViewChild('owlElement') owlElement: OwlCarousel;
+  carouselSelector : number;
+  projects = [
+    {id: 1, title:'Superman'},
+    {id: 2, title:'Batman'},
+    {id: 5, title:'BatGirl'},
+    {id: 3, title:'Robin'},
+    {id: 4, title:'Flash'}
   ];
-  
+
+  stuff(){
+    console.log(this.carouselSelector);
+    this.owlElement.to([3,300]);
+  }
+
+  hoverSelect(index) {
+    this.owlElement.to([index,300]);
+  }
+
   constructor() { }
 
   ngOnInit() {
